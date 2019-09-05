49a81f9797578f40f7f10ac0313b793c8d2d1fe9
russell-west-twin-1996 <54961931+russell-west-twin-1996@users.noreply.github.com>
2019-09-05 17:09:22 -0400

    Create Homework.cpp

---
+++ Homework.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+  int length = 5;
+  int width = 7;
+  double area;
+  
+  area = length * width;
+  cout << "The area of the Rectangle is " << area << endl;
+  
+ return 0;
+ 
+ }
